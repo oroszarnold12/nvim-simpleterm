7fd0e05625c2396f278834ab08de9305f9ea0356
oroszarnold <arnold.orosz@onedot.com>
2024-09-29 18:50:31 +0300

    * fixed formatting

--- lua/simpleterm/init.lua
+++ lua/simpleterm/init.lua
@@ -1,83 +1,83 @@
 local M = {}
 
 local defaults = {
-  type_opts = {
-    horizontal = { location = "rightbelow", split_ratio = 0.3 },
-    vertical = { location = "rightbelow", split_ratio = 0.5 },
-    floating = {
-      relative = "editor",
-      row = 0.3,
-      col = 0.25,
-      width = 0.5,
-      height = 0.4,
-      border = "single",
-    },
-  },
-  behavior = {
-    autoclose_on_quit = {
-      enabled = false,
-      confirm = true,
-    },
-    close_on_exit = true,
-    auto_insert = true,
-  },
+	type_opts = {
+		horizontal = { location = "rightbelow", split_ratio = 0.3 },
+		vertical = { location = "rightbelow", split_ratio = 0.5 },
+		floating = {
+			relative = "editor",
+			row = 0.3,
+			col = 0.25,
+			width = 0.5,
+			height = 0.4,
+			border = "single",
+		},
+	},
+	behavior = {
+		autoclose_on_quit = {
+			enabled = false,
+			confirm = true,
+		},
+		close_on_exit = true,
+		auto_insert = true,
+	},
 }
 
 local set_behavior = function(behavior)
-  if behavior.autoclose_on_quit.enabled then
-    local function close_buffers()
-      require("simpleterm.terminal").close_buffers()
-      vim.api.nvim_command "qa"
-    end
-    vim.api.nvim_create_autocmd({ "WinClosed" }, {
-      callback = vim.schedule_wrap(function()
-        local open_term_windows = require("simpleterm.terminal").get_windows()
+	if behavior.autoclose_on_quit.enabled then
+		local function close_buffers()
+			require("simpleterm.terminal").close_buffers()
+			vim.api.nvim_command("qa")
+		end
+		vim.api.nvim_create_autocmd({ "WinClosed" }, {
+			callback = vim.schedule_wrap(function()
+				local open_term_windows = require("simpleterm.terminal").get_windows()
 
-        local non_term_windows = vim.tbl_filter(function(win)
-          return not vim.tbl_contains(open_term_windows, win)
-        end, vim.api.nvim_list_wins())
+				local non_term_windows = vim.tbl_filter(function(win)
+					return not vim.tbl_contains(open_term_windows, win)
+				end, vim.api.nvim_list_wins())
 
-        if not vim.tbl_isempty(non_term_windows) then
-          return
-        end
+				if not vim.tbl_isempty(non_term_windows) then
+					return
+				end
 
-        if not behavior.autoclose_on_quit.confirm then
-          return close_buffers()
-        end
+				if not behavior.autoclose_on_quit.confirm then
+					return close_buffers()
+				end
 
-        vim.ui.input({ prompt = "Close all terms and quit? (Y/N): " }, function(input)
-          if not input or not string.lower(input) == "y" then
-            return
-          end
-          close_buffers()
-        end)
-      end),
-    })
-  end
+				vim.ui.input({ prompt = "Close all terms and quit? (Y/N): " }, function(input)
+					if not input or not string.lower(input) == "y" then
+						return
+					end
+					close_buffers()
+				end)
+			end),
+		})
+	end
 
-  if behavior.close_on_exit then
-    vim.api.nvim_create_autocmd({ "TermClose" }, {
-      callback = function()
-        vim.schedule_wrap(vim.api.nvim_input "<CR>")
-      end,
-    })
-  end
+	if behavior.close_on_exit then
+		vim.api.nvim_create_autocmd({ "TermClose" }, {
+			callback = function()
+				vim.schedule_wrap(vim.api.nvim_input("<CR>"))
+			end,
+		})
+	end
 
-  if behavior.auto_insert then
-    vim.api.nvim_create_autocmd({ "BufEnter" }, {
-      callback = function()
-        vim.cmd "startinsert"
-      end,
-      pattern = "term://*",
-    })
-  end
+	if behavior.auto_insert then
+		vim.api.nvim_create_autocmd({ "BufEnter" }, {
+			callback = function()
+				vim.cmd("startinsert")
+			end,
+			pattern = "term://*",
+		})
+	end
 end
 
 M.setup = function(config)
-  config = config and vim.tbl_deep_extend("force", defaults, config) or defaults
+	config = config and vim.tbl_deep_extend("force", defaults, config) or defaults
 
-  set_behavior(config.behavior)
-  require("simpleterm.terminal").init(config)
+	set_behavior(config.behavior)
+	require("simpleterm.terminal").init(config)
 end
 
 return M
